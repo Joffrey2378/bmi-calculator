bde7fa5a339af40fd631cc584426af059a5b5016
Joffrey2378 <georged.2378@gmail.com>
2019-10-07 08:43:42 +0300

    Constants file w/ styles and colors was added

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kInactiveContainerColor = Color(0xFF111328);
+const kActiveContainerColor = Color(0xFF1D1E33);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(color: Color(0xFF8D8E98));
+const kNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+const kIconSize = 70.0;
+const kSizedBoxHeight = 10.0;
+
+const kActiveTrackColor = Colors.white;
+const kInactiveTrackColor = Color(0xFF8D8E98);
+const kThumbColor = Color(0xFFEB1555);
+const kOverlayColor = Color(0x29EB1555);
+const kThumbRadius = RoundSliderThumbShape(enabledThumbRadius: 12.0);
+const kOverlayRadius = RoundSliderOverlayShape(overlayRadius: 22.0);
